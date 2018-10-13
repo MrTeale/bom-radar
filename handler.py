fccd5bb244a3657705361cff85c2ec520f8ce981
Lachlan Teale <lachlan.teale@gmail.com>
2018-10-13 14:23:15 +1000

    Inital Commit

---
+++ handler.py
@@ -0,0 +1,62 @@
+import json
+import re
+from io import BytesIO
+
+import numpy as np
+import requests
+from bs4 import BeautifulSoup
+
+from PIL import Image
+from flask import send_file
+
+
+def main(event, context):
+
+    page_link = 'http://www.bom.gov.au/products/IDR503.loop.shtml'
+
+    locations_link = 'http://www.bom.gov.au/products/radar_transparencies/IDR503.locations.png'
+    topography_link = 'http://www.bom.gov.au/products/radar_transparencies/IDR503.topography.png'
+    range_link = 'http://www.bom.gov.au/products/radar_transparencies/IDR503.range.png'
+    background_link = 'http://www.bom.gov.au/products/radar_transparencies/IDR503.background.png'
+
+    page_response = requests.get(page_link)
+    soup = BeautifulSoup(page_response.text, features='html.parser')
+    image_names = soup.find_all("script")[8]
+    p = re.compile(r'theImageNames\[[0-9]{1,3}\] = "(.*?)";')
+    gif_links = p.findall(str(image_names))
+
+    gif_images = []
+
+    for link in gif_links:
+        response = requests.get(link)
+        gif_images.append(Image.open(BytesIO(response.content)).convert("RGBA"))
+
+    response = requests.get(range_link)
+    range_image = Image.open(BytesIO(response.content)).convert("RGBA")
+
+    response = requests.get(locations_link)
+    locations_image = Image.open(BytesIO(response.content)).convert("RGBA")
+
+    response = requests.get(topography_link)
+    topography_image = Image.open(BytesIO(response.content)).convert("RGBA")
+
+    response = requests.get(background_link)
+    background_image = Image.open(BytesIO(response.content)).convert("RGBA")
+
+    created_images = []
+
+    for image in gif_images:
+        new_image = Image.alpha_composite(background_image, topography_image)
+        new_image = Image.alpha_composite(new_image, image)
+        new_image = Image.alpha_composite(new_image, range_image)
+        new_image = Image.alpha_composite(new_image, locations_image)
+        
+        created_images.append(new_image)
+
+    created_images[0].save('radar.gif',
+                save_all=True,
+                append_images=created_images[1:],
+                duration=400,
+                loop=0)
+
+    return send_file('radar.gif', mimetype='image/gif')
